62322c1407421684cf7fcf1cb9400fbbbde1ec15
Guy Hughes <g@gxg.me>
2015-11-27 18:18:42 -0500

    idscraper recurses on next button

--- etl/idscraper/idscraper/spiders/alpha.py
+++ etl/idscraper/idscraper/spiders/alpha.py
@@ -1,19 +1,27 @@
 # -*- coding: utf-8 -*-
-import scrapy
-import re
+import scrapy,re
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider,Rule
 from idscraper.items import AlphaItem
-class AlphaSpider(scrapy.Spider):
+
+class AlphaSpider(CrawlSpider):
     name = "alpha"
-    allowed_domains = ["tripadvisor.ca","*"]
+    allowed_domains = ["tripadvisor.ca","tripadvisor.com"]
     start_urls = [ "http://www.tripadvisor.ca/Restaurants-g155004-Ottawa_Ontario.html" ]
 
-    def parse(self, response):
+    #scrapy.spiders.Rule(link_extractor, callback=None, cb_kwargs=None, follow=None, process_links=None, process_request=None)
+    rules = (
+        Rule(LinkExtractor(restrict_xpaths=("//a[contains(concat(' ', normalize-space(@class), ' '), ' next')]"),allow=('.*',)),follow=True,callback='parse_item'),
+    ) 
+
+    def parse_item(self, response):
+        self.logger.info('Parsing URL %s', response.url )
         for restaurant in response.xpath('//div[@id="EATERY_SEARCH_RESULTS"]//a[@class="property_title"]'):
             item = AlphaItem()
             print restaurant
             item['name']=restaurant.xpath('text()').extract()
             item['url']=restaurant.xpath('@href').extract()
-            item['id']= restaurant.xpath('@href').re('.*-d([\d]{7})-.*')
+            item['id']= restaurant.xpath('@href').re('.*-d([\d]{1,7})-.*')
             #  <a target="_blank" href="/Restaurant_Review-g2690985-d4494962-Reviews-Wiches_Cauldron-Stittsville_Ottawa_Ontario.html" class="property_title" onclick="ta.restaurant_list_tracking.clickDetailTitle('/Restaurant_Review-g2690985-d4494962-Reviews-Wiches_Cauldron-Stittsville_Ottawa_Ontario.html','tags_category_tag_restaurants','4494962','1','46');">
             #  'Wiches Cauldron
             #  </a>'
