1602b97e588849642f5b3a0ca2554de97a0a7824
Jean-Philippe Daigle <jdaigle@tripadvisor.com>
2015-11-28 10:56:46 -0500

    support for TA IDs, photos, and web url

--- etl/api_processor/api_fetcher.py
+++ etl/api_processor/api_fetcher.py
@@ -4,7 +4,7 @@ import fileinput
 import time
 # list of ids from STDIN -> all relevant api info into files
 
-def api_parser(file_name_location, location_info, review_info):
+def api_parser(id, file_name_location, location_info, review_info, photos_info):
 
 	data = json.loads(location_info)
 	data_reviews = json.loads(review_info)
@@ -28,16 +28,32 @@ def api_parser(file_name_location, location_info, review_info):
 	if 'rating' in data and data['rating']:
 		rating = float(data['rating'])
 		
+	# grab first photo from the photos data
+	photo_url = None
+	photos_obj = json.loads(photos_info)['data']
+	if len(photos_obj) > 0:
+		# attempt to grab first blessed photo (if exists) otherwise pick all
+		blessed_photos = [x for x in photos_obj if x['is_blessed']]
+		if len(blessed_photos) == 0:
+			blessed_photos = photos_obj
+		
+		photo1_obj = blessed_photos[0]
+		if len(photo1_obj['images']) > 0:
+			photo_url = photo1_obj['images']['small']['url']
+	
 
 	# filtering location to only fields we care about
 	data_temp = {
+				'ta_id': int(id),
 				'placetype': "restaurant",
 				'address': address,
 				'coordinates': coords,
 				'rating': rating,
 				'name': data['name'],
 				'price_level': data['price_level'],
-				'reviews': review_array
+				'reviews': review_array,
+				'photo_url': photo_url,
+				'web_url': data['web_url']
 				}
 
 	with open(file_name_location, 'w') as outfile:
@@ -61,7 +77,12 @@ def api_fetcher(id_list):
 			url = ('http://api.tripadvisor.com/api/partner/2.0/location/%s/reviews?key=A9778200-77E7-4FD8-86B1-240E13A873F4' % id)
 			reviews_info = urllib2.urlopen(url)
 			reviews_info = reviews_info.read()
-			api_parser(file_name_location, location_info, reviews_info)
+			
+			url = ('http://api.tripadvisor.com/api/partner/2.0/location/%s/photos?key=A9778200-77E7-4FD8-86B1-240E13A873F4' % id)
+			photos_info = urllib2.urlopen(url)
+			photos_info = photos_info.read()
+			
+			api_parser(id, file_name_location, location_info, reviews_info, photos_info)
 
 		except Exception as e:
 			print("ERR: %s" % e)
