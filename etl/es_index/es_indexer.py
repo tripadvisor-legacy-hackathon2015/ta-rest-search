5a3cd0c452364667149977342639ba4567a68b17
Jean-Philippe Daigle <jdaigle@tripadvisor.com>
2015-11-28 09:44:22 -0500

    added args parsing and optional url arg

--- etl/es_index/es_indexer.py
+++ etl/es_index/es_indexer.py
@@ -6,8 +6,19 @@ import sys
 import requests
 import json
 import time
+import argparse
 
 BASEURL = "http://es-hack-2.dai.gl:9200"
+INDEXNAME = "locationidx"
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--url')
+parser.add_argument('files', nargs='*')
+args = parser.parse_args()
+
+if args.url:
+    # override default
+    BASEURL = args.url
 
 def fmt_response(resp):
     if not resp:
@@ -16,7 +27,7 @@ def fmt_response(resp):
 
 def create_index():
     print("Creating index")
-    res = requests.post(BASEURL + '/locationidx')
+    res = requests.post(BASEURL + '/' + INDEXNAME)
     print(fmt_response(res))
     time.sleep(3)
 
@@ -27,21 +38,21 @@ def create_mapping():
             "coordinates": {"type": "geo_point"}
         }
     }
-    res = requests.post(BASEURL + '/locationidx/_mapping/location', data=json.dumps(mapping_props))
+    res = requests.post(BASEURL + '/%s/_mapping/location' % INDEXNAME, data=json.dumps(mapping_props))
     print(fmt_response(res))
     time.sleep(3)
 
 create_index()
 create_mapping()
 
-for fname in sys.argv[1:]:
+for fname in args.files:
     print(fname)
     
     with open(fname, 'r') as infile:
         restaurant_obj = json.load(infile)
         
         try:
-            res = requests.post(BASEURL + '/locationidx/location', data=json.dumps(restaurant_obj))
+            res = requests.post(BASEURL + '/%s/location' % INDEXNAME, data=json.dumps(restaurant_obj))
             print(fmt_response(res))
         except Error as e:
             print("ERR: %s" % e)
