06fdad89a7ed081ba1318ac68674d7dc499b5446
Guy Hughes <g@gxg.me>
2015-11-27 19:41:56 -0500

    idscraper complete ☺

--- etl/idscraper/idscraper/spiders/alpha.py
+++ etl/idscraper/idscraper/spiders/alpha.py
@@ -18,13 +18,17 @@ class AlphaSpider(CrawlSpider):
         self.logger.info('Parsing URL %s', response.url )
         for restaurant in response.xpath('//div[@id="EATERY_SEARCH_RESULTS"]//a[@class="property_title"]'):
             item = AlphaItem()
-            print restaurant
+            #print restaurant
             item['name']=restaurant.xpath('text()').extract()
             item['url']=restaurant.xpath('@href').extract()
-            item['id']= restaurant.xpath('@href').re('.*-d([\d]{1,7})-.*')
+            item['id']= restaurant.xpath('@href').re('.*-d([\d]{1,9})-.*')
+            #  print item
+            #  print item['url']
+            if not u'-g155004-' in str(item['url']):
+                continue
+            print str(item['id'][0])
             #  <a target="_blank" href="/Restaurant_Review-g2690985-d4494962-Reviews-Wiches_Cauldron-Stittsville_Ottawa_Ontario.html" class="property_title" onclick="ta.restaurant_list_tracking.clickDetailTitle('/Restaurant_Review-g2690985-d4494962-Reviews-Wiches_Cauldron-Stittsville_Ottawa_Ontario.html','tags_category_tag_restaurants','4494962','1','46');">
             #  'Wiches Cauldron
             #  </a>'
-            print item
             yield item
         pass
